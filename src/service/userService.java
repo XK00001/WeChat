a62c03028c37e6cd831bc7e72c96be7e7d6e8693
XK00001 <514726090@qq.com>
2022-06-23 16:33:22 +0800

    del

--- src/service/userService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import java.sql.SQLException;
-import pojo.user;
-
-public interface userService {
-	
-	public user userLogin(String username,String password) throws SQLException;
-}
